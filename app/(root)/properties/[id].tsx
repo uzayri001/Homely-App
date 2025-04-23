ba76549575205c9c54f9cf84e39a7726602c6ebb
Uzayr <uzayr001@gmail.com>
2025-04-23 13:59:11 +0200

    display property name

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,15 +1,26 @@
-import { View, Text, SafeAreaView, ScrollView, Image, Dimensions, FlatList } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image, Dimensions, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 import Carousel from 'react-native-reanimated-carousel';
 import { getPropertyById } from '@/lib/appwrite';
 import { useAppwrite } from '@/lib/useAppWrite';
 import images from '@/constants/images';
+import icons from '@/constants/icons';
 
+interface Property {
+  $id: string;
+  name?: string;
+  imageUrls: string[];
+  address?: string;
+}
+
+interface PropertyParams extends Record<string, string | number> {
+  id: string;
+}
 
 const Property = () => {
   const {id} = useLocalSearchParams<{id: string}>();
-  const {data: property} = useAppwrite({fn: getPropertyById, params: {id: id!},});
+  const {data: property} = useAppwrite<Property | null, PropertyParams>({fn: getPropertyById, params: {id: id!},});
   const windowHeight = Dimensions.get("window").height;
   const windowWidth = Dimensions.get("window").width;
 
@@ -17,6 +28,25 @@ const Property = () => {
     <SafeAreaView className="bg-white">
       <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName="pb-32 bg-white">
         <View className="relative w-full" style={{ height: windowHeight / 2 }}>
+          <View className="absolute top-4 left-4 z-10">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="bg-white p-2 rounded-full">
+                <Image source={icons.backArrow} className="w-6 h-6"/>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View className="absolute top-4 right-4 z-10 flex flex-row gap-2">
+            <View className="p-2 rounded-full bg-white">
+              <Image
+                source={icons.heart}
+                className="size-7"
+                tintColor={"#191D31"}
+              />
+            </View>
+            <View className="p-2 rounded-full bg-white">
+              <Image source={icons.send} className="size-7" tintColor={"#191D31"}/>
+            </View>
+          </View>
           <Carousel
             data={property?.imageUrls || []}
             renderItem={({ item }) => (
@@ -36,6 +66,12 @@ const Property = () => {
               overflow: 'hidden'
             }}
           />
+        </View>
+        <View className="flex flex-row items-center justify-between px-4">
+          <View className="flex flex-row items-center gap-2">
+            <Text className="text-2xl font-rubik-ExtraBold text-black-300">{property?.name}</Text>
+            <Text className="text-sm font-rubik-Medium text-black-200">{property?.address}</Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
